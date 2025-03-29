12acc62ffd000e2b7d02f8bbea778bfb645b4948
Matyáš Bubrinka <56973449+matesiskocz@users.noreply.github.com>
2025-03-29 20:31:02 +0100

    feat(UoW): added unit of work and unit of work factory

---
+++ ICS_Project.DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using ICS_Project.DAL.Entities;
+using ICS_Project.DAL.Repositories;
+using ICS_Project.DAL.Mappers;
+
+namespace ICS_Project.DAL.UnitOfWork;
+
+public interface IUnitOfWork : IAsyncDisposable
+{
+    IRepository<TEntity> GetRepository<TEntity, TEntityMapper>()
+        where TEntity : class, IEntity
+        where TEntityMapper : IEntityMapper<TEntity>, new();
+
+    Task CommitAsync();
+}
