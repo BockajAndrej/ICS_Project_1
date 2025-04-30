1bf9d87f7c4374a573cc09d7461cdb712bf8f6bf
Burr3to <filoj.jf@gmail.com>
2025-04-30 16:59:11 +0200

    feat(maui): Initialize new Maui project
    Creates a new Maui application within the `maui` subdirectory of the current project.

---
+++ ICS_Project.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ICS_Project.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ICS_Project.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ICS_Project.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
