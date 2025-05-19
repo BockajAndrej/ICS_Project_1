8ace2125767336a1ced4265ed4f305368687bc8d
Vít Kološ <xkolosv00@stud.fit.vutbr.cz>
2025-05-19 16:59:47 +0200

    feat: Introduce functionality for music tracks adding
    Edit mode is not functional yet.
    Added MusicTrackEditGUID message required by edit mode.
    MusicTrack duration GUI field reworked to better accept valid timespan values.
    Added support methods to validate given time lenght.
    Added whole funcionality for displaying and filtering genres and authors to choose from.
    Added command for saving and revertign changes.
    In popup xaml fixed broken scrolling by setting grid row size to '*' and disabeling option to close popup by clicking outside of it.
    Rerouted eddit button in music track list.
    Added methods to add/remove authors and genres from musicTrack to MusicTrackFacade.
    Added property isSelected to both ArtistListModel and GenreListModel.

--- ICS_Project.App/Views/MusicTrack/Popups/MusicTrackEditView.xaml.cs
+++ ICS_Project.App/Views/MusicTrack/Popups/MusicTrackEditView.xaml.cs
@@ -1,14 +1,27 @@
 using ICS_Project.App.ViewModels.MusicTrack;
 using CommunityToolkit.Maui.Views;
-using ICS_Project.App.ViewModels.MusicTrack; // Ensure this using is correct
+using ICS_Project.App.ViewModels.MusicTrack;
+using System.Diagnostics;
+using CommunityToolkit.Mvvm.Messaging;
+using ICS_Project.App.Messages; 
 
 namespace ICS_Project.App.Views.MusicTrack.Popups;
 
 public partial class MusicTrackEditView : Popup
 {
-	public MusicTrackEditView(MusicTrackDetailViewModel viewModel)
+    private readonly IServiceProvider _serviceProvider;
+	public MusicTrackEditView(MusicTrackDetailViewModel viewModel, IServiceProvider serviceProvider)
 	{
 		InitializeComponent();
         BindingContext = viewModel;
+        _serviceProvider = serviceProvider;
+    }
+
+        // Handler for the Edit button
+        private void EditButton_Clicked(object sender, EventArgs e)
+    {
+        Debug.WriteLine("Edit button clicked!");
+       
+        throw new NotImplementedException("Edit functionality is not implemented yet.");
     }
 }
